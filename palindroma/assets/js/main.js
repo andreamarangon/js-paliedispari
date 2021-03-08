ada6afaed64de4e0c06450cd258f68b4c31fe917
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-08 18:06:05 +0100

    esercizio terminato

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,9 +1,11 @@
 //chiedo all'utente di inserire una parola
 var parola = prompt('Scrivi una parola');
 
+//stampo il risultato nel DOM nell'elemento con id risultato
 document.getElementById('risultato').innerHTML = palindrome(parola);
 
 //creo funzione
+//FUNCTIONS
 function palindrome (word){
   if(word.split("").reverse().join("") === word) {
      return ('La parola è palindroma');
--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -1,39 +1,58 @@
-//chiedo all'utente di scegliere pari o dispari
-var pariOdispari = prompt('p=pari, d= dispari');
-console.log(pariOdispari);
+//chiedo all'utente di scegliere pari o dispari e lo salvo in variabile
+var pariOdispari = prompt('Scegli p=pari o d=dispari');
+
+// stampo nel Dom la risposta nell'elemento con id pari-dispari
+if (pariOdispari=='p') {
+  document.getElementById('pari-dispari').innerHTML = 'Hai scelto pari';
+} else if (pariOdispari=='d'){
+  document.getElementById('pari-dispari').innerHTML = 'Hai scelto dispari';
+} else {
+  document.getElementById('pari-dispari').innerHTML = 'Non mi hai dato un dato corretto!';
+
+}
 
 //chiedo all'utente di inserire un numero da 1 a 5
 var numeroUtente = parseInt(prompt('Dimmi un numero  da 1 a 5'));
-console.log(numeroUtente);
 
-//genero numero random da 1 a 5 per computer
+// stampo nel Dom la risposta nell'elemento con id numero_utente
+document.getElementById('numero_utente').innerHTML = 'Il tuo numero è: ' + numeroUtente;
+
+//genero numero random da 1 a 5 per computer tramite la funzione
 var numeroComputer = randomNumber(1, 5);
-console.log(numeroComputer);
 
+// stampo nel Dom la risposta nell'elemento con id numero_computer
+document.getElementById('numero_computer').innerHTML = 'Il numero del computer è: ' + numeroComputer;
 
 //stabilisco se la somma è pari o dispari con funzione
 var pd = evenOdd(numeroUtente, numeroComputer);
-console.log(pd);
 
-//dichiaro chi ha vinto
-if (pariOdispari==pd) {
-  console.log('hai vinto');
+//stampo se la somma dei numeri è pari o dispari nell'elemento con id risultato_somma
+if (pd == 'p') {
+  document.getElementById('risultato_somma').innerHTML = 'La somma dei numeri è pari';
 } else {
-  console.log('hai perso');
+  document.getElementById('risultato_somma').innerHTML = 'La somma dei numeri è dispari';
 }
+
+//dichiaro chi ha vinto stampando il risultato nel Dom nell'elemento con id risultato
+if (pariOdispari==pd) {
+  document.getElementById('risultato').innerHTML = 'Hai vinto';
+} else {
+  document.getElementById('risultato').innerHTML = 'Hai perso';
+}
+
 //FUNCTIONS
 
-//numero random
+//creo una funzione per generare un numero random
 function randomNumber(min, max) {
   var numero = Math.floor(Math.random() * (max - min + 1) + min);
   return numero;
 }
 
-//pari o dispari
+//creo una funzione per stabilire se la somma di due numeri è pari o dispari
 function evenOdd(num1, num2) {
   if ((num1 + num2) % 2 == 0 ) {
-    return 'p';
+    return 'p';//pari
   } else {
-    return 'd';
+    return 'd';//dispari
   }
 }
