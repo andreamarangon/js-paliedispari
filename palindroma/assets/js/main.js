06d323000f6c5bc4cee330e76882fabefd29a507
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-08 17:24:42 +0100

    lavoro su palindroma

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,4 +1,14 @@
 //chiedo all'utente di inserire una parola
-var str = prompt('Scrivi una parola');
+var parola = prompt('Scrivi una parola');
+
+document.getElementById('risultato').innerHTML = palindrome(parola);
 
 //creo funzione
+function palindrome (word){
+  if(word.split("").reverse().join("") === word) {
+     return ('La parola è palindroma');
+  } else {
+    return ('La parola non è palindroma');
+  }
+
+}
